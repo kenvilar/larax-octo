fa502b26098e93ef1d5b291a2a66054a791e009c
Ken Vilar <kenvilar@gmail.com>
2018-03-27 19:35:20 +0800

    Create Plugin class for larax custom plugin

---
+++ plugins/larax/resources/Plugin.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace Larax\Resources;
+
+class Plugin {
+	public function pluginDetails() {
+		return [
+			'name' => 'Larax',
+			'description' => 'Larax-Octo custom plugin',
+			'author' => 'Ken Vilar',
+			'icon' => 'icon-leaf'
+		];
+	}
+}
